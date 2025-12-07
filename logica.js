895eb2c803dec2e20539e1a2d6aca69ab76b85de
Hugo Méndez Fernández <145331195+uo288543@users.noreply.github.com>
2025-12-07 20:17:46 +0100

    Update logica.js

--- logica.js
+++ logica.js
@@ -78,7 +78,7 @@ async function search(aBuscar) {
         //crear un enlace a la página correspondiente por cada resultado encontrado
         results.forEach(item => {
             const link = document.createElement('a');
-            link.href = 'ux' + item.url;
+            link.href = item.url;
             link.textContent = item.text + ' (' + 'ux' + item.url + ')';
             resultsContainer.appendChild(link);
         });
@@ -103,5 +103,6 @@ deleteBtn.addEventListener('click', () => {
     }
 
 });
+
 
 
